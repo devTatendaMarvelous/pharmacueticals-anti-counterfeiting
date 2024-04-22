f4c00603c430a874181352c3af425c95aa938d0c
devTatendaMarvelous <marveloustchimusoro@gmail.com>
2024-04-22 20:58:02 +0200

    QR code scanning

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -40,7 +40,11 @@ class ProductsController extends Controller
                 "manufactured_date" => "required",
                 "expiry_date" => "required"
             ]);
+            if ($product['serial']<6 and $product['serial']>9 ){
 
+                Toastr::error('Batch number must be between 6 and 9', 'error');
+                return back();
+            }
             $product['manufacturer_id'] = auth()->user()->manufacturer->id;
             if ($request->has('product_photo')) {
                 $product['product_photo'] = $request->file('product_photo')->store('productPhotos', 'public');
@@ -82,6 +86,12 @@ class ProductsController extends Controller
                 "expiry_date" => "required"
 
             ]);
+
+            if ($product['serial']<6 and $product['serial']>9 ){
+
+                Toastr::error('Batch number must be between 6 and 9', 'error');
+                return back();
+            }
 
             if ($request->has('product_photo')) {
 
